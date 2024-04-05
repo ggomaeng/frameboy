16d9ee7f515d7b88a0cf6c1a2e067503937ece7c
0xggoma <sungwoopark95@gmail.com>
2024-04-05 14:25:00 +0900

    update deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,29 +4,20 @@ git pull && npm version patch && git push &&
 
 TIMESTAMP=`date +%Y-%m-%d-%H-%M-%S` &&
 RELEASE_DIR="/srv/web/frameboy.xyz" &&
-BUN="/home/ubuntu/.bun/bin/bun" &&
+BUN="/home/updatebot/.bun/bin/bun" &&
 
 echo "Copy .env.production to the shared directory and link..." &&
-scp .env.production oracle:$RELEASE_DIR/.env &&
+scp .env.production mintclub:$RELEASE_DIR/.env &&
 
-ssh oracle "
+ssh mintclub "
   cd $RELEASE_DIR &&
   git checkout bun.lockb &&
   git pull &&
-  $BUN install &&
-  npx prisma migrate deploy &&
-  npx prisma generate &&
-  mkdir .next-$TIMESTAMP &&
-  BUILD_DIR=".next-$TIMESTAMP" npm run build &&
-  ln -sfn .next-$TIMESTAMP .next" &&
+  $BUN install" &&
 
 echo "Reloading process..." &&
-ssh oracle "pm2 reload derivative-art --update-env --node-args='--no-warnings=ExperimentalWarning'" &&
+ssh mintclub "pm2 reload frameboy.xyz --update-env --node-args='--no-warnings=ExperimentalWarning'" &&
 
 echo "_________" &&
-echo "Deploy completed successfully on $TIMESTAMP" &&
-
-echo "Cleaning up old releases..." &&
-ssh oracle "find $RELEASE_DIR/.next-20* -mtime +3 -type d -exec rm -rf {} \;" &&
-ssh oracle "df -lh | grep root" 
+echo "Deploy completed successfully on $TIMESTAMP"
 
